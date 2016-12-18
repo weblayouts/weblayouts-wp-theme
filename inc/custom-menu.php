8bbf18a6a44abe7f2c449fdaf7b208f918821606
Eric Njanga <ericnjanga@Erics-MacBook-Pro.local>
2016-12-18 09:36:01 -0500

    Adding documentation on "Walker_menu_cat_filter"

--- inc/custom-menu.php
+++ inc/custom-menu.php
@@ -26,30 +26,34 @@
 
 
 
+	/**
+	 * Walker operating on menu's anchor tags in two ways:
+	 * 1) Adding the "button" classes
+	 * 2) Adding "data-filter" attribute
+	*/
+  class Walker_menu_cat_filter extends Walker { 
+    // Tell Walker where to inherit it's parent and id values
+    var $db_fields = array(
+        'parent' => 'menu_item_parent', 
+        'id'     => 'db_id' 
+    );
 
-    class Walker_menu_cat_filter extends Walker { 
-        // Tell Walker where to inherit it's parent and id values
-        var $db_fields = array(
-            'parent' => 'menu_item_parent', 
-            'id'     => 'db_id' 
+    /**
+     * Add "data-filter" attribute for every link.
+     * This will contain category's hyphenated title
+     */
+    function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) { 
+      if( $args->menu->name == 'Menu of Categories' ){
+        $cat_filter = str_replace(' ', '-', strtolower ( $item->title )); 
+        $output .= sprintf( "\n<li><a data-filter='category-%s' href='%s' class='button button--small button--round button--yde cat-filter %s'>%s</a></li>\n",
+          $cat_filter,
+          $item->url,
+          ( strtolower($item->title) === 'all' ) ? 'current' : '',
+          $item->title
         );
-
-        /**
-         * Add "data-filter" attribute for every link.
-         * This will contain category's hyphenated title
-         */
-        function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) { 
-            if( $args->menu->name == 'Menu of Categories' ){
-                $cat_filter = str_replace(' ', '-', strtolower ( $item->title )); 
-                $output .= sprintf( "\n<li><a data-filter='category-%s' href='%s' class='button button--small button--round button--yde cat-filter %s'>%s</a></li>\n",
-                    $cat_filter,
-                    $item->url,
-                    ( strtolower($item->title) === 'all' ) ? 'current' : '',
-                    $item->title
-                );
-            } 
-        } 
-    }
+    	} 
+    } 
+  }
 
 
 ?>
